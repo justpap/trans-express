61d51d070036bde354e47287431224364eb96f90
Justyna Paprzycka <justynaciszek7@gmail.com>
2019-01-25 13:34:56 +0100

    Dodanie adresu w sekcji contact

--- js/scroll-reveal.js
+++ js/scroll-reveal.js
@@ -7,7 +7,7 @@ sr.reveal('.header-text, .text2-header', {
     reset: true,
     delay: 100
 });
-sr.reveal('p, .fab, .text-list', {
+sr.reveal('p, span, i, .fab, .text-list', {
     reset: true,
     delay: 300
 });
